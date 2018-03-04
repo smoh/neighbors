5eff8668ae8328a238c2e6b9092ca635421cab96
Semyeong Oh <semyeong.oh@gmail.com>
2018-03-04 15:40:47 -0500

    collect visualization routines; added a new plotting func

---
+++ neighbors/visualization/__init__.py
@@ -0,0 +1,3 @@
+'''
+Module for visualization routines
+'''
--- neighbors/visualization/vis.py
+++ neighbors/visualization/vis.py
@@ -2,7 +2,9 @@
 import numpy as np
 import palettable
 from matplotlib.collections import LineCollection, PatchCollection
+from matplotlib.patches import Ellipse
 from matplotlib import pyplot as plt
+
 from isochrones.mist import MIST_Isochrone
 
 __all__ = [
@@ -10,6 +12,8 @@ __all__ = [
     'get_isodf',
     'plotcmd_mist_isochrones',
     'violinplot',
+    'pairplot',
+    'plot_cov_ellipse'
 ]
 
 # phase color dictionary for MIST isochrones
@@ -132,3 +136,41 @@ def pairplot(x1, y1, x2, y2, ax=None, colors=None, linewidths=1, **kwargs):
     ax = plt.gca() if not ax else ax
     ax.add_collection(lines)
     return lines
+
+
+def plot_cov_ellipse(cov, pos, nstd=2, ax=None, **kwargs):
+    """
+    Plots an `nstd` sigma error ellipse based on the specified covariance
+    matrix (`cov`). Additional keyword arguments are passed on to the
+    ellipse patch artist.
+    Parameters
+    ----------
+        cov : The 2x2 covariance matrix to base the ellipse on
+        pos : The location of the center of the ellipse. Expects a 2-element
+            sequence of [x0, y0].
+        nstd : The radius of the ellipse in numbers of standard deviations.
+            Defaults to 2 standard deviations.
+        ax : The axis that the ellipse will be plotted on. Defaults to the
+            current axis.
+        Additional keyword arguments are pass on to the ellipse patch.
+    Returns
+    -------
+        A matplotlib ellipse artist
+    """
+    def eigsorted(cov):
+        vals, vecs = np.linalg.eigh(cov)
+        order = vals.argsort()[::-1]
+        return vals[order], vecs[:,order]
+
+    if ax is None:
+        ax = plt.gca()
+
+    vals, vecs = eigsorted(cov)
+    theta = np.degrees(np.arctan2(*vecs[:,0][::-1]))
+
+    # Width and height are "full" widths, not radius
+    width, height = 2 * nstd * np.sqrt(vals)
+    ellip = Ellipse(xy=pos, width=width, height=height, angle=theta, **kwargs)
+
+    ax.add_artist(ellip)
+    return ellip
