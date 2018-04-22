d35afc1208382c93e715fdd483e0f7b6d61c900a
Adrian Price-Whelan <adrian.prw@gmail.com>
2018-04-22 14:42:15 -0400

    Add class to simplify computing likelihood ratios

--- neighbors/modelselection/fml.py
+++ neighbors/modelselection/fml.py
@@ -1,25 +1,17 @@
+# Standard library
+import math
+
 # Third-party
+import astropy.units as u
+from astropy.utils.misc import isiterable
 import numpy as np
-from scipy.special import logsumexp
 from scipy.integrate import simps
 
 # Project
+from .data import data_to_vec_cov
 from .likelihood import ln_marg_v_likelihood
 
-__all__ = ['ln_H1_FML', 'ln_H2_FML']
-
-
-rlim = 2000. # pc; HACK: hard-coded limit. Deal With It!
-A = np.log(3) - 3*np.log(rlim) - 0.5 * np.log(2 * np.pi)
-def ln_prob(r, plx, err):
-    """
-    Evaluate log(likelihood * prior). This assumes a uniform space density
-    prior.
-    """
-    B = -np.log(err)
-    C = -0.5 * (1000 / r - plx)**2 / err**2
-    D = 2 * np.log(r)
-    return A + B + C + D
+__all__ = ['ComovingFML']
 
 
 def get_mode(plx, err):
@@ -28,41 +20,153 @@ def get_mode(plx, err):
     return 1000 * snr**2 / (4 * plx) * (1 - np.sqrt(1 - 8 / snr**2))
 
 
-def get_rgrid(plx, err, ngrid=25):
-    return np.linspace(1000/(plx + 4*err),
-                       min(rlim, 1000/(plx - 4*err)),
+def get_rgrid(plx, err, rlim, ngrid=25):
+    """
+    Get a uniform grid of true distance to compute the fully marginalized
+    likelihood for a star or set of stars.
+    """
+    return np.linspace(1000 / (plx + 4*err),
+                       min(rlim, 1000 / (plx - 4*err)),
                        ngrid)
 
 
-def ln_H1_FML(data1, Cov1, data2, Cov2, Vinvs, prior_weights,
-              n_dist_grid=32, v_scatter=0.):
-    # TODO: need to construct a 2D d1, d2 grid here!
-    ll_H1_at_samples = np.array([ln_H1_marg_v_likelihood(d1, d2, star1, star2, Vinv, v_scatter, prior_weights=prior_weights)
-                                 for d1, d2 in zip(dist1, dist2)])
-    return logsumexp(ll_H1_at_samples) - np.log(float(ll_H1_at_samples.size))
+# First term in the log-likelihood + log-prior evaluation
+_const = math.log(3) - 0.5 * math.log(2 * np.pi)
+
+def ln_dist_prob(r, plx, err, rlim):
+    """
+    Evaluate log(likelihood * prior) for true distance, ``r``. This assumes
+    a uniform space density prior.
+
+    Parameters
+    ----------
+    TODO
+
+    """
+    A = _const - 3 * math.log(rlim)
+    B = -math.log(err)
+    C = -0.5 * ((1000 / r - plx) / err)**2
+    D = 2 * math.log(r)
+    return A + B + C + D
 
 
-##############################################################################
-#  Likelihood 2:
-#
-def ln_integrand2(r, data, Cov, Vinvs, prior_weights):
-    ln_fr = ln_marg_v_likelihood([r], data, Cov, Vinvs,
-                                 prior_weights=prior_weights)
-    ln_p = ln_prob(r, data[0, 2], np.sqrt(Cov[0, 2, 2]), rlim)
-    return ln_fr + ln_p
+def ln_dist_integrand(rs, data, Cov, v_prior, rlim, v_scatter):
+    """
+    Integrand for the distance marginalization for one or two stars.
 
+    Parameters
+    ----------
+    TODO
 
-def ln_Q(data, Cov, Vinvs, prior_weights, n_dist_grid):
-    rgrid = get_rgrid(data[0, 2], np.sqrt(Cov[0, 2, 2]), n_dist_grid)
-    func_vals = [ln_integrand2(r, data, Cov, Vinvs, prior_weights)
-                 for r in rgrid]
-    return simps(np.exp(func_vals), x=rgrid)
+    """
+    ln_f = ln_marg_v_likelihood(rs, data, Cov, prior=v_prior,
+                                v_scatter=v_scatter)
+    ln_p = [ln_dist_prob(rs[i], data[i, 2], np.sqrt(Cov[i, 2, 2]), rlim)
+            for i in range(len(rs))]
+    return ln_f + sum(ln_p)
 
+class ComovingFML:
+    """
+    Helper class for computing the two fully marginalized likelihoods used for
+    selecting candidate comoving stars.
 
-def ln_H2_FML(star1, star2, Vinv, n_dist_samples=128, v_scatter=0.,
-              prior_weights=None):
-    dist1 = get_posterior_distance_samples(star1, size=n_dist_samples)
-    dist2 = get_posterior_distance_samples(star2, size=n_dist_samples)
-    ll_H2_at_samples = np.array([ln_H2_marg_v_likelihood(d1, d2, star1, star2, Vinv, v_scatter, prior_weights=prior_weights)
-                                 for d1,d2 in zip(dist1, dist2)])
-    return logsumexp(ll_H2_at_samples) - np.log(float(ll_H2_at_samples.size))
+    Parameters
+    ----------
+    gaia_data : ``GaiaData`` instance
+        TODO
+    v_prior : dict
+        TODO
+    v_scatter : `~astropy.units.Quantity`, optional [km/s]
+        TODO
+    rlim : `~astropy.units.Quantity`, optional [kpc]
+        Maximum true distance to use for the assumed uniform space density prior
+        over distance.
+    n_dist_grid : int, iterable, optional
+        The number of grid points to used when computing the marginalization
+        over true distance. This can either be an integer (same number of grid
+        points for both stars), or an iterable (number of grid points for each
+        star). If not specified, this is estimated automatically for each star.
+    """
+    @u.quantity_input(v_scatter=u.km/u.s, rlim=u.kpc)
+    def __init__(self, gaia_data, v_prior,
+                 v_scatter=0*u.km/u.s, rlim=2*u.kpc, n_dist_grid=None):
+        # Retrieve and convert data for each star
+        data, Cov = data_to_vec_cov(gaia_data)
+        if data.shape[0] != 2:
+            raise ValueError("This class currently only supports pairs of "
+                             "stars. You passed in data for {0} stars."
+                             .format(data.shape[0]))
+
+        if data.shape != Cov.shape[:2]:
+            raise ValueError("Covariance matrix has invalid shape.")
+
+        self.data = data
+        self.Cov = Cov
+
+        self.rlim = rlim
+        self._rlim = self.rlim.to(u.pc).value
+
+        self.v_scatter = v_scatter
+        self._v_scatter = self.v_scatter.to(u.km/u.s).value
+
+        self.v_prior = v_prior
+
+        if n_dist_grid is None:
+            # HACK: TODO: estimate this automatically using some heuristic
+            n_dist_grid = (25, 25)
+
+        elif isiterable(n_dist_grid):
+            n_dist_grid = tuple(map(int, n_dist_grid))
+
+        else:
+            try:
+                n_dist_grid = (int(n_dist_grid), int(n_dist_grid))
+            except Exception as e:
+                raise TypeError("Invalid specification of number of distance "
+                                "grid points: {0}".format(n_dist_grid))
+
+        self.n_dist_grid = n_dist_grid
+
+        # parallax at index 2, parallax error: note that we drop the cross
+        # terms!!!
+        rgrid1 = get_rgrid(self.data[0, 2], np.sqrt(self.Cov[0, 2, 2]),
+                           self._rlim, self.n_dist_grid[0])
+        rgrid2 = get_rgrid(self.data[1, 2], np.sqrt(self.Cov[1, 2, 2]),
+                           self._rlim, self.n_dist_grid[1])
+        self.rgrids = [rgrid1, rgrid2]
+
+    ###########################################################################
+    # Likelihood 1:
+    #
+    def ln_H1_FML(self):
+        ln_H = np.array([ln_dist_integrand([r1, r2], self.data, self.Cov,
+                                           self.v_prior, self._rlim,
+                                           self._v_scatter)
+                         for r1 in self.rgrids[0] for r2 in self.rgrids[1]])
+        ln_H = ln_H.reshape(self.n_dist_grid)
+
+        # Simpson's rule, twice to do 2D integral:
+        return np.log(simps(simps(np.exp(ln_H), x=self.rgrids[0], axis=0),
+                            x=self.rgrids[1]))
+
+    ###########################################################################
+    # Likelihood 2:
+    #
+    def ln_Q(self, index):
+        """
+        Compute the log(Q) integral (Eq. 10) for the star with the specified
+        index in the internal data array.
+        """
+        data = self.data[index]
+        Cov = self.Cov[index]
+        rgrid = self.rgrids[index]
+        func_vals = [ln_dist_integrand([r], data[None], Cov[None],
+                                       self.v_prior, self._rlim,
+                                       self._v_scatter)
+                     for r in rgrid]
+        return np.log(simps(np.exp(func_vals), x=rgrid))
+
+    def ln_H2_FML(self):
+        ln_Q1 = self.ln_Q(index=0)
+        ln_Q2 = self.ln_Q(index=1)
+        return ln_Q1 + ln_Q2
